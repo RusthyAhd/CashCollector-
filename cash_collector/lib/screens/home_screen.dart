ec1b195cdd91a17eae942d606a424dc89f84c8e7
Himashirathnayake1 <himashirathnayake182@gmail.com>
2025-05-14 15:16:12 +0530

    lost total paid after 12 hours

--- cash_collector/lib/screens/home_screen.dart
+++ cash_collector/lib/screens/home_screen.dart
@@ -148,7 +148,77 @@ class _RoutePageState extends State<RoutePage> {
         onRefresh: _fetchTotalPaidAcrossAllRoutes,
         child: Column(
           children: [
-            Padding(
+            Expanded(
+              child: FutureBuilder<QuerySnapshot>(
+                future: FirebaseFirestore.instance.collection('routes').get(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+
+                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    return const Center(child: Text("No routes found"));
+                  }
+
+                  final routes = snapshot.data!.docs;
+
+                  return ListView.separated(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 24),
+                    itemCount: routes.length,
+                    separatorBuilder: (_, __) => const SizedBox(height: 16),
+                    itemBuilder: (context, index) {
+                      final doc = routes[index];
+                      final routeName = doc.id;
+                      final emoji = "🛣️";
+
+                      return Material(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        elevation: 3,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) =>
+                                    ShopListScreen(routeName: routeName),
+                              ),
+                            );
+                          },
+                          borderRadius: BorderRadius.circular(12),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 16),
+                            child: Row(
+                              children: [
+                                Text(emoji,
+                                    style: const TextStyle(fontSize: 24)),
+                                const SizedBox(width: 12),
+                                Expanded(
+                                  child: Text(
+                                    routeName,
+                                    style: const TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black87,
+                                    ),
+                                  ),
+                                ),
+                                const Icon(Icons.arrow_forward_ios_rounded,
+                                    size: 18, color: Colors.grey),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            const Divider(),
+             Padding(
               padding: const EdgeInsets.all(16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -296,83 +366,22 @@ class _RoutePageState extends State<RoutePage> {
                         ),
                       );
                     },
-                    child: const Text("Submit Receipt & Amount"),
+                    child: Center(child: const Text("Submit Receipt & Amount", style: TextStyle(color: Colors.black),)),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 113, 182, 116),
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-            const Divider(),
-            Expanded(
-              child: FutureBuilder<QuerySnapshot>(
-                future: FirebaseFirestore.instance.collection('routes').get(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-
-                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                    return const Center(child: Text("No routes found"));
-                  }
-
-                  final routes = snapshot.data!.docs;
-
-                  return ListView.separated(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16, vertical: 24),
-                    itemCount: routes.length,
-                    separatorBuilder: (_, __) => const SizedBox(height: 16),
-                    itemBuilder: (context, index) {
-                      final doc = routes[index];
-                      final routeName = doc.id;
-                      final emoji = "🛣️";
-
-                      return Material(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                        elevation: 3,
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) =>
-                                    ShopListScreen(routeName: routeName),
-                              ),
-                            );
-                          },
-                          borderRadius: BorderRadius.circular(12),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 20, horizontal: 16),
-                            child: Row(
-                              children: [
-                                Text(emoji,
-                                    style: const TextStyle(fontSize: 24)),
-                                const SizedBox(width: 12),
-                                Expanded(
-                                  child: Text(
-                                    routeName,
-                                    style: const TextStyle(
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.w500,
-                                      color: Colors.black87,
-                                    ),
-                                  ),
-                                ),
-                                const Icon(Icons.arrow_forward_ios_rounded,
-                                    size: 18, color: Colors.grey),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
           ],
+          
         ),
+        
       ),
     );
   }
